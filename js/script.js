5aeaa85781114a8cab02849d821e2ab20240c55e
RodrigoMattosOSF <rodrigo.mattos@osf-commerce.com>
2019-02-12 18:08:57 -0200

    DWTR-18285 - Created the list functionalities as the exercise objective

--- js/script.js
+++ js/script.js
@@ -1,50 +1,96 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
+   recordsList its the base for the controllers and paginations.
+   recordsPage contains the records for each page for the current script
+   currentPage helps to control which page is currenty displaying
 ***/
+const recordsList   = document.querySelector('.student-list');
+var recordsPerPage  = 10;
+var currentPage     = 1;
+
+/**
+ * Loop through the records and displays only the ones between the page range
+ *
+ * @param {Int} page
+ * @param {HTMLCollection} list
+ */
+var showPage = (page, list) => {    
+    let pageLimit = page * recordsPerPage;
+    let pageStart = (page - 1) * recordsPerPage;
+
+    for (let r = 0; r < list.length; r++) {
+        let record = list[r];
+        if (r >= pageStart && r < pageLimit) {
+            record.style.display = 'block';
+        }else{
+            record.style.display = 'none';
+        }
+    }
+};
+
+/**
+ * Configure the initial pagination then displays the page passed as a parameter with the amount of records desired per page
+ *
+ * @param {Int} page
+ */
+var appendPageLinks = (page) => {    
+    let listParent          = recordsList.parentNode;
+    let paginationContainer = listParent.querySelector('.pagination');
+
+    //Case the pagination already exists we should remove it and create a new element for the configuration
+    if (paginationContainer) {
+        listParent.removeChild(paginationContainer);
+    }
+    
+    paginationContainer           = document.createElement('div');
+    paginationContainer.className = 'pagination';    
+
+    //Create the actual list container to add each link
+    let paginationList = document.createElement('ul');
+    let pagesNumber    = Math.ceil(recordsList.children.length / recordsPerPage);
+        
+    //Create the links and adds them to the page
+    for (let p = 1; p <= pagesNumber; p++) {
+        let li = document.createElement('li');
+        let a  = document.createElement('a');
+
+        if (p == page) {
+            a.className = 'active';
+        }
+
+        a.textContent = p;
+        li.appendChild(a);
+        paginationContainer.appendChild(li);
+    }
+
+    listParent.appendChild(paginationContainer);
+
+    //Add the events for changing pages
+    paginationContainer.addEventListener('click', (event) => {
+        let element = event.target;
+        
+        if (element.tagName == 'A') {
+            if (!element.classList.contains('active')) {
+                let parentPagination = element.parentNode.parentNode;
+                let linkList         = parentPagination.querySelectorAll('a');
+
+                for (let a = 0; a < linkList.length; a++) {
+                    let link = linkList[a];
+                    link.removeAttribute('class');
+                }
+
+                element.className = 'active';
+                currentPage       = element.textContent;
+                showPage(currentPage, recordsList.children);
+                
+            }
+        }
+        
+    });
+
+    //Display initial configured page
+    showPage(page, recordsList.children);
+};
 
 
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+//Executes the initial function to render the page list with the corret configuration
+appendPageLinks(currentPage);
